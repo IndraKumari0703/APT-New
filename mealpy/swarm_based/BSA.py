fd2b29eb19add6b39a86f8a46c3e0eef653bf8bf
IndraKumari0703 <137739158+IndraKumari0703@users.noreply.github.com>
2023-07-11 02:13:18 +0900

    Update BSA.py

--- mealpy/swarm_based/BSA.py
+++ mealpy/swarm_based/BSA.py
@@ -1,9 +1,3 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 11:59, 17/03/2020 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
-
 import numpy as np
 from copy import deepcopy
 from mealpy.optimizer import Optimizer

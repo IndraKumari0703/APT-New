2c235e2460415120977df20301b6bd9f54fc9882
IndraKumari0703 <137739158+IndraKumari0703@users.noreply.github.com>
2023-07-11 18:55:41 +0900

    Update EHO.py

--- mealpy/swarm_based/EHO.py
+++ mealpy/swarm_based/EHO.py
@@ -1,8 +1,3 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 18:41, 08/04/2020 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy

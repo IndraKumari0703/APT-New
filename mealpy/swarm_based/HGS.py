796ec52cfc160615271c74229a74004d011c8fbf
IndraKumari0703 <137739158+IndraKumari0703@users.noreply.github.com>
2023-07-11 19:01:48 +0900

    Update HGS.py

--- mealpy/swarm_based/HGS.py
+++ mealpy/swarm_based/HGS.py
@@ -1,8 +1,4 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 15:37, 19/03/2021 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
+
 
 import numpy as np
 from copy import deepcopy

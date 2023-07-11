56b3aa7e17b7ef108a762c590fb0516146480d9c
IndraKumari0703 <137739158+IndraKumari0703@users.noreply.github.com>
2023-07-11 19:03:51 +0900

    Update SSA.py

--- mealpy/swarm_based/SSA.py
+++ mealpy/swarm_based/SSA.py
@@ -1,8 +1,4 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 17:22, 29/05/2020 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
+
 
 import numpy as np
 from copy import deepcopy

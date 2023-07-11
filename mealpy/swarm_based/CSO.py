ebcb3202e6e1aa7be271c06bc12e21c35af563d3
IndraKumari0703 <137739158+IndraKumari0703@users.noreply.github.com>
2023-07-11 18:55:26 +0900

    Update CSO.py

--- mealpy/swarm_based/CSO.py
+++ mealpy/swarm_based/CSO.py
@@ -1,8 +1,3 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 10:09, 17/03/2020 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy

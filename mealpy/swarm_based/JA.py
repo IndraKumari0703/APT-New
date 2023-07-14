54bbf57bb723635b4a6a9d51907f1b890d1d3b59
IndraKumari0703 <137739158+IndraKumari0703@users.noreply.github.com>
2023-07-14 11:25:02 +0900

    Update JA.py

--- mealpy/swarm_based/JA.py
+++ mealpy/swarm_based/JA.py
@@ -1,9 +1,3 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 16:30, 16/11/2020 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
-
 import numpy as np
 from mealpy.optimizer import Optimizer
 
